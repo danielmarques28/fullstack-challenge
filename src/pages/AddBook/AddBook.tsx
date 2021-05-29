28fd9d3bfe5de2d8fb4b288f92953b8fc59b3922
Daniel Marques <danielmarques7@hotmail.com>
2021-05-28 23:19:48 -0300

    Create AddBook page

---
+++ src/pages/AddBook/AddBook.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container } from 'src/components';
+
+export const AddBook = () => {
+  return (
+    <Container>
+      <View>
+        <Text>AddBook</Text>
+      </View>
+    </Container>
+  );
+};
