39523e173250d2c07da5104ec10ccd7e0d488585
Daniel Marques <danielmarques7@hotmail.com>
2021-05-31 00:41:06 -0300

    Add FlatList of BookItem in Home page

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const HomeView = styled.View``;
+export const HomeView = styled.View`
+  ${({ theme }) => `
+    padding: ${theme.spacings.small};
+  `}
+`;
+
+export const ItemView = styled.View`
+  ${({ theme }) => `
+    flex: 1;
+    flex-direction: column;
+    padding-bottom: ${theme.spacings.small};
+  `}
+`;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Container } from 'src/components';
+import { FlatList } from 'react-native';
+import { BookItem, Box, Container, Text } from 'src/components';
+import { useTheme } from 'styled-components';
 import * as S from './Home.styles';
 
+import { StackNavigationProp } from '@react-navigation/stack';
+import { HomeStackParamList } from 'src/navigation/stacks';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+type HomeNavigationProp = StackNavigationProp<HomeStackParamList, 'Home'>;
+
 export const Home = () => {
+  const theme = useTheme();
+  const navigation = useNavigation<HomeNavigationProp>();
+
   return (
-    <Container>
+    <Container scrollView={false}>
       <S.HomeView>
-        <Text>Home</Text>
+        <Text color="primary" variant="title">
+          Hi, <Text variant="title">Mehmed Al Fatih</Text>
+        </Text>
+
+        <Box height={theme.spacings.medium} />
+
+        <FlatList
+          data={Array(4).fill({
+            id: 1,
+            image: 'src/assets/images/png/book.png',
+            title: 'The One Thing',
+            author: 'Gary Keller',
+          })}
+          renderItem={({ item }) => (
+            <S.ItemView>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Details', { id: item.id })}
+              >
+                <BookItem
+                  image={require('src/assets/images/png/book.png')}
+                  title={item.title}
+                  author={item.author}
+                />
+              </TouchableOpacity>
+            </S.ItemView>
+          )}
+          numColumns={3}
+          keyExtractor={(index) => index}
+          refreshing={false}
+          onRefresh={() => {}}
+        />
       </S.HomeView>
     </Container>
   );
