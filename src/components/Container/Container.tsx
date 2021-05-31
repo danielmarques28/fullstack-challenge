b71bea41d1c776b6e556d7600f0517f97e7edf96
Daniel Marques <danielmarques7@hotmail.com>
2021-05-31 00:33:27 -0300

    Add scrollView prop in Container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,24 +5,29 @@ import * as S from './Container.styles';
 
 type ContainerProps = {
   children: React.ReactNode;
+  scrollView?: boolean;
 };
 
-export const Container = ({ children }: ContainerProps) => {
+export const Container = ({ children, scrollView = true }: ContainerProps) => {
   return (
     <SafeAreaView>
       <S.ContainerView>
         <StatusBar barStyle="light-content" />
 
-        <KeyboardAwareScrollView
-          alwaysBounceVertical={false}
-          enableOnAndroid={true}
-          contentContainerStyle={{
-            flexGrow: 1,
-          }}
-          keyboardShouldPersistTaps="handled"
-        >
-          {children}
-        </KeyboardAwareScrollView>
+        {scrollView ? (
+          <KeyboardAwareScrollView
+            alwaysBounceVertical={false}
+            enableOnAndroid={true}
+            contentContainerStyle={{
+              flexGrow: 1,
+            }}
+            keyboardShouldPersistTaps="handled"
+          >
+            {children}
+          </KeyboardAwareScrollView>
+        ) : (
+          <>{children}</>
+        )}
       </S.ContainerView>
     </SafeAreaView>
   );
