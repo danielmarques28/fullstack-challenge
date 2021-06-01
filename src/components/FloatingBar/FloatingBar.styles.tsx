ccb6d0f73809d04123a4ddac56d9a45c18a72cd6
Daniel Marques <danielmarques7@hotmail.com>
2021-06-01 00:22:34 -0300

    Create FloatingBar component

---
+++ src/components/FloatingBar/FloatingBar.styles.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const FloatingBarView = styled.View`
+  ${({ theme }) => `
+    position: absolute;
+    padding: ${theme.spacings.small} ${theme.spacings.xxxsmall};
+    bottom: ${theme.spacings.small};
+    justify-content: space-between;
+    align-self: center;
+    flex-direction: row;
+    background-color: white;
+    border-radius: ${theme.spacings.xxsmall};
+    shadow-color: #000;
+    shadow-opacity: 0.8;
+    elevation: 1;
+  `}
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+  ${({ theme }) => `
+    flex-direction: row;
+    padding: 0 ${theme.spacings.small};
+  `}
+`;
+
+export const BorderV = styled.View`
+  ${({ theme }) => `
+    border-left-width: 1px;
+    opacity: 0.25;
+    border-left-color: ${theme.colors.gray};
+  `}
+`;
