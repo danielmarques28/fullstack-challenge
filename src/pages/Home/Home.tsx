efa748061f95ffcbfa41e3e12c962c550090449a
Daniel Marques <danielmarques7@hotmail.com>
2021-06-02 02:39:16 -0300

    Add refresh and onEndReached in Home FlatList

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList } from 'react-native';
 import { BookItem, Box, Container, Text } from 'src/components';
 import { useTheme } from 'styled-components';
 import * as S from './Home.styles';
-
 import { StackNavigationProp } from '@react-navigation/stack';
 import { HomeStackParamList } from 'src/navigation/stacks';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import { ListBooks, listBooks } from 'src/api/book';
+import { isFloat, isInt } from 'src/utils/calcule';
 
-type HomeNavigationProp = StackNavigationProp<HomeStackParamList, 'Home'>;
+export type HomeNavigationProp = StackNavigationProp<
+  HomeStackParamList,
+  'Home'
+>;
 
 export const Home = () => {
   const theme = useTheme();
   const navigation = useNavigation<HomeNavigationProp>();
+
+  const [books, setBooks] = useState<ListBooks['rows']>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    handleListBooks();
+  }, []);
+
+  const handleListBooks = useCallback(async () => {
+    console.log('handleListBooks');
+
+    try {
+      setRefreshing(true);
+
+      const { data } = await listBooks();
+
+      setBooks(data.rows);
+      setCount(data.count);
+
+      setRefreshing(false);
+    } catch (error) {
+      setRefreshing(false);
+      console.log('error', error);
+    }
+  }, []);
+
+  const loadMoreBooks = async (newPage: number) => {
+    try {
+      setIsLoading(true);
+      setPage(newPage);
+
+      const { data } = await listBooks(newPage);
+      console.log(data.rows);
+
+      setBooks([...books, ...data.rows]);
+      setCount(data.count);
+
+      setRefreshing(false);
+    } catch (error) {
+      setRefreshing(false);
+      console.log('error', error);
+    }
+  };
 
   return (
     <Container scrollView={false}>
@@ -25,31 +74,49 @@ export const Home = () => {
         <Box height={theme.spacings.medium} />
 
         <FlatList
-          data={Array(4).fill({
-            id: 1,
-            image: 'src/assets/images/png/book.png',
-            title: 'The One Thing',
-            author: 'Gary Keller',
-          })}
+          data={books}
           renderItem={({ item }) => (
             <S.ItemView>
-              <TouchableOpacity
-                onPress={() =>
-                  navigation.navigate('BookDetails', { id: item.id })
-                }
-              >
-                <BookItem
-                  image={require('src/assets/images/png/book.png')}
-                  title={item.title}
-                  author={item.author}
-                />
-              </TouchableOpacity>
+              <BookItem
+                image={require('src/assets/images/png/book.png')}
+                id={item.id}
+                name={item.name}
+                author={item.author}
+                navigation={navigation}
+              />
             </S.ItemView>
           )}
           numColumns={3}
-          keyExtractor={(index) => index}
-          refreshing={false}
-          onRefresh={() => {}}
+          keyExtractor={(item) => String(item.id)}
+          refreshing={refreshing}
+          onRefresh={() => {
+            setPage(1);
+            handleListBooks();
+          }}
+          onEndReached={() => {
+            const limit = 10;
+            const total = count / limit;
+
+            if (
+              (isInt(total) && page >= total) ||
+              (isFloat(total) && page >= ~~total + 1)
+            ) {
+              setIsLoading(false);
+              return;
+            }
+
+            loadMoreBooks(page + 1);
+          }}
+          onEndReachedThreshold={0}
+          ListFooterComponent={() => (
+            <>
+              {isLoading && (
+                <S.LoadingView>
+                  <ActivityIndicator size="large" color={theme.colors.black} />
+                </S.LoadingView>
+              )}
+            </>
+          )}
         />
       </S.HomeView>
     </Container>
